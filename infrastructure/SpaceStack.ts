246817da9fad80929b8336745579138f238f7e9b
teixeira-matheus <teixeirammatheus@gmail.com>
2021-10-06 15:28:29 -0500

    installing aws-sdk

--- infrastructure/SpaceStack.ts
+++ infrastructure/SpaceStack.ts
@@ -1,6 +1,6 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { LambdaIntegration, RestApi } from 'aws-cdk-lib/lib/aws-apigateway';
-import { Code, Function as LambdaFunction, Runtime } from 'aws-cdk-lib/lib/aws-lambda';
+import { PolicyStatement } from 'aws-cdk-lib/lib/aws-iam';
 import { NodejsFunction } from 'aws-cdk-lib/lib/aws-lambda-nodejs';
 import { Construct } from 'constructs';
 import { join } from 'path';
@@ -18,20 +18,21 @@ export class SpaceStack extends Stack {
   constructor(scope: Construct, id: string, props: StackProps){
     super(scope, id, props);
 
-    const helloLambda = new LambdaFunction(this, 'helloLambda', {
-      runtime: Runtime.NODEJS_14_X,
-      code: Code.fromAsset(join(__dirname, '..', 'services', 'hello', 'hello.js')),
-      handler: 'hello.main'
-    });
-
     const helloLambdaNodeJs = new NodejsFunction(this, 'helloLambdaNodeJs', {
       entry: join(__dirname, '..', 'services', 'node-lambda', 'hello.ts'),
       handler: 'handler'
     });
 
+    //add policy to allow lambda to list buckets
+    const s3ListPolicy = new PolicyStatement();
+    s3ListPolicy.addActions('s3:ListAllMyBuckets');
+    s3ListPolicy.addResources('*');
+    helloLambdaNodeJs.addToRolePolicy(s3ListPolicy);
+
     // Hello APi gateway lambda integration
-    const helloLambdaIntegration = new LambdaIntegration(helloLambda);
+    const helloLambdaIntegration = new LambdaIntegration(helloLambdaNodeJs);
     const helloLambdaResource = this.api.root.addResource('hello');
     helloLambdaResource.addMethod('GET', helloLambdaIntegration);
+    
   }
 }
