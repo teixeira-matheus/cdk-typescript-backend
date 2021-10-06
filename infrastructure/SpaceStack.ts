578ce267dbc4df4634714179af7aec15a73c1918
teixeira-matheus <teixeirammatheus@gmail.com>
2021-10-06 09:26:47 -0500

    21 - Bootstrapping cdk project and add a lambda function

---
+++ infrastructure/SpaceStack.ts
@@ -0,0 +1,25 @@
+import { Stack, StackProps } from 'aws-cdk-lib';
+import { LambdaIntegration, RestApi } from 'aws-cdk-lib/lib/aws-apigateway';
+import { Code, Function as LambdaFunction, Runtime } from 'aws-cdk-lib/lib/aws-lambda';
+import { Construct } from 'constructs';
+import { join } from 'path';
+
+export class SpaceStack extends Stack {
+
+  private api = new RestApi(this, 'SpaceApi')
+
+  constructor(scope: Construct, id: string, props: StackProps){
+    super(scope, id, props);
+
+    const helloLambda = new LambdaFunction(this, 'helloLambda', {
+      runtime: Runtime.NODEJS_14_X,
+      code: Code.fromAsset(join(__dirname, '..', 'services', 'hello')),
+      handler: 'hello.main'
+    });
+
+    // Hello APi lambda integration
+    const helloLambdaIntegration = new LambdaIntegration(helloLambda);
+    const helloLambdaResource = this.api.root.addResource('hello');
+    helloLambdaResource.addMethod('GET', helloLambdaIntegration);
+  }
+}
